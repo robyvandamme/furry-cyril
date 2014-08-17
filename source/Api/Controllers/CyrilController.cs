01bcad040d4d9865da4fd93a145486523743a1aa
robyvandamme <roby.vandamme@irobyx.com>
2014-08-17 16:00:23 +0200

    Added minimal Web Api

---
+++ source/Api/Controllers/CyrilController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Web.Http;
+using Domain.Model;
+
+namespace Api.Controllers
+{
+    public class CyrilController : ApiController
+    {
+        // GET api/cyril
+        public IEnumerable<Cyril> Get()
+        {
+            return new List<Cyril> { new Cyril(){Id="1", Name="Cyril", Age = 5, Color = "Black", Options = new Collection<CyrilOption>(){new CyrilOption(){Option = "Blah", OptionId = 512}}} };
+        }
+
+        // GET api/cyril/1
+        public Cyril Get(int id)
+        {
+            return new Cyril() { Id = "1", Name = "Cyril", Age = 5, Color = "Black", Options = new Collection<CyrilOption>() { new CyrilOption() { Option = "Blah", OptionId = 512 }}} ;
+        }
+    }
+}
---
+++ source/Api/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Api.Startup))]
+
+namespace Api
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseWebApi(WebApiConfig.Register());
+        }
+    }
+}
